10c7aef84e6f44c7a97f29b461ee2d47ca3898b7
Dmitry Baev <charlie@yandex-team.ru>
2015-04-17 14:41:27 +0300

    init commit

---
+++ src/main/java/com/github/baev/MyPlugin.java
@@ -0,0 +1,48 @@
+package com.github.baev;
+
+import ru.yandex.qatools.allure.data.AllureTestCase;
+import ru.yandex.qatools.allure.data.plugins.Plugin;
+import ru.yandex.qatools.allure.data.plugins.PluginData;
+import ru.yandex.qatools.allure.data.plugins.ProcessPlugin;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Dmitry Baev charlie@yandex-team.ru
+ *         Date: 17.04.15
+ */
+@Plugin.Name("my-plugin")
+public class MyPlugin implements ProcessPlugin<AllureTestCase> {
+
+    public void process(AllureTestCase data) {
+        //do nothing
+    }
+
+    public List<PluginData> getPluginData() {
+        return Collections.singletonList(
+                new PluginData("my-plugin.json", new MyPluginContent("MY_PLUGIN_CONTENT"))
+        );
+    }
+
+    public Class<AllureTestCase> getType() {
+        return AllureTestCase.class;
+    }
+
+    public static class MyPluginContent {
+
+        private String field;
+
+        public MyPluginContent(String field) {
+            this.field = field;
+        }
+
+        public String getField() {
+            return field;
+        }
+
+        public void setField(String field) {
+            this.field = field;
+        }
+    }
+}
